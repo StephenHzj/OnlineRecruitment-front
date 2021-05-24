05431267cea2daafd5d96f13f2938fe5c8490749
StephenHe <852630479@qq.com>
2021-05-24 22:41:24 +0800

    牛马

---
+++ src/components/user/Application.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="16" offset="4">
+        <el-table
+            :data="AppData"
+            style="width: 100%"
+           >
+          <el-table-column
+              prop="applicationDate"
+              label="日期"
+              sortable
+              width="180">
+          </el-table-column>
+          <el-table-column
+              prop="jobName"
+              label="岗位名称"
+              width="180">
+          </el-table-column>
+          <el-table-column
+              prop="hrName"
+              label="处理人"
+              width="180">
+          </el-table-column>
+          <el-table-column
+              prop="companyName"
+              label="公司名称"
+              width="180">
+          </el-table-column>
+          <el-table-column
+              prop="companyState"
+              label="投递状态"
+              align="center">
+            <template slot-scope="scope">
+              <el-tag
+                  :type="scope.row.applicationState === 1?'success':(scope.row.applicationState=== -1?'danger':'info')"
+              >{{scope.row.companyState === 1? '已通过' : (scope.row.companyState === -1 ? '已拒绝':'未审核')}}</el-tag>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+  </div>
+
+</template>
+
+<script>
+import * as userApi from "../../api/user"
+export default {
+  name: "Application",
+  created() {
+    this.getAppList();
+  },
+  methods: {
+    getAppList(){
+      let userTel = localStorage.getItem("userTel");
+      userApi.getAppList(userTel).then(res => {
+        if(res.code === 200){
+           this.AppData =  res.data
+        }
+      })
+    },
+  },
+  data() {
+    return {
+      AppData: []
+    }
+  }
+}
+</script>
+<style>
+.el-table .warning-row {
+  background: #F56C6C;
+}
+
+.el-table .success-row {
+  background: #67C23A;
+}
+</style>
---
+++ src/components/user/UserInfo.vue
@@ -0,0 +1,109 @@
+<template>
+  <el-card  class="box-card">
+    <div slot="header" class="clearfix">
+      <span>个人信息</span>
+    </div>
+    <div  class="text item">
+      <el-form :model="user" status-icon :rules="rules" ref="userRegister" label-width="100px"  class="demo-ruleForm">
+
+        <el-form-item label="手机号" prop="userTel">
+          <el-input v-model="user.userTel"></el-input>
+        </el-form-item>
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="user.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="userPassword">
+          <el-input type="password" v-model="user.userPassword" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="userEmail">
+          <el-input v-model="user.userEmail"></el-input>
+        </el-form-item>
+
+        <el-form-item label="性别" prop="sex" >
+          <el-radio-group v-model="user.userGender">
+            <el-radio-button label="1">男</el-radio-button>
+            <el-radio-button label="0">女</el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="生日" prop="userBirth">
+          <el-date-picker
+              v-model="user.userBirth"
+              type="date"
+              placeholder="选择日期"
+              value-format="yyyy-MM-dd">
+          </el-date-picker>
+        </el-form-item>
+
+        <el-form-item label="用户Logo">
+          <el-upload
+              action="/api/user/logo/upload"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload">
+            <el-avatar v-if="imageUrl" :src="imageUrl"></el-avatar>
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="register()">修改信息</el-button>
+        </el-form-item>
+
+      </el-form>
+    </div>
+  </el-card>
+</template>
+
+<script>
+import * as userApi from "@/api/user"
+export default {
+  name: "UserInfo",
+  data(){
+    return{
+      user: {
+        userId: "",
+        userTel: "",
+        userName: "",
+        userPassword: "",
+        userLogo: "",
+        userGender: "",
+        userEmail: "",
+        userBirth: "",
+      },
+      imageUrl:"",
+    }
+  },
+
+  created() {
+    this.getUserInfo();
+  },
+  methods:{
+    getUserInfo(){
+      let userTel = localStorage.getItem("userTel")
+      userApi.getUserInfo(userTel).then(res => {
+        if(res.code === 200 ){
+          this.user = res.data;
+        }
+      })
+    },
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+      console.log(this.imageUrl);
+      console.log(file.name)
+      this.company.companyLogo =res.data
+    },
+    beforeAvatarUpload(file) {
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      return isLt2M;
+    },
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
